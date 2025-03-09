3bdb45027ab5eab3b385eec2e50305a6fabf3f9d
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2025-03-08 23:28:41 -0300

    Add files via upload

---
+++ ImprimeNumerosLoop.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ImprimeNumerosLoop
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+
+            for(int i=1; i < 10; i++){
+
+            	Console.WriteLine("Número: " + i);
+
+			}      
+       
+		}
+	}
+}
