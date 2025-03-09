6fea4c427963c35659a379a9f9a9fd41ba934d86
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2025-03-08 23:32:22 -0300

    Add files via upload

---
+++ ImprimeNumerosLoopInverso.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ImprimeNumerosLoopInverso
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+
+            for(int i=10; i > 0; i--){
+
+            	Console.WriteLine("Número: " + i);
+
+			}      
+       
+		}
+	}
+}
